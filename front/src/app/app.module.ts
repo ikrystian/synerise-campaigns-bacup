4b40c819e1a12629b171deffb1a888efe23ba562
Krystian <krystian@bpcoders.pl>
2021-02-03 23:38:52 +0100

    Add search engine

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { LoginComponent } from './login/login.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,7 @@ import { MatInputModule } from '@angular/material/input';
     HomeComponent,
     CampaignComponent,
     SingleCampaignComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -51,6 +52,7 @@ import { MatInputModule } from '@angular/material/input';
     MatSelectModule,
     MatInputModule,
     ReactiveFormsModule,
+    Ng2SearchPipeModule
   ],
   providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -5,17 +5,20 @@ import { environment } from '../../environments/environment';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
 
   data: any;
+  searchText: any;
   constructor(private http: HttpClient) {
     this.getRepos();
   }
 
   getRepos() {
-    return this.http.get(`${environment.apiUrl}/show.php`).subscribe(response => this.data = response);
+    return this.http.get(`${environment.apiUrl}/show.php`).subscribe(response => {
+      this.data = response;
+    });
   }
 
 }
